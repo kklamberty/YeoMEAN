ca6f5b8bab8de2b253b0e3d4aed8bc76b2d7d8a2
kklamberty <lamberty@morris.umn.edu>
2014-09-25 12:54:06 -0500

    updated teh controller to add courses

--- client/app/gpacalc/gpacalc.controller.js
+++ client/app/gpacalc/gpacalc.controller.js
@@ -2,5 +2,28 @@
 
 angular.module('yeoMeanApp')
   .controller('GpacalcCtrl', function ($scope, $http) {
-    $scope.message = 'Hello';
-  });
+        $scope.courseList = [];
+
+        //Update the courseList to show the data already in the database
+        $http.get('/api/courses').success(function(courseList) {
+            $scope.courseList = courseList;
+        });
+
+
+        $scope.addCourse = function () {
+            if ($scope.newCourse === '' || $scope.newGrade === '' || $scope.newCourseCredits ==='') {
+                return;
+            }
+            $http.post('/api/courses', { name: $scope.newCourse, grade: $scope.newGrade, gradepoints : 4, credits: $scope.newCourseCredits}).success(function () {
+                //Update movieList to have the same data that's in the database on the sever
+                $http.get('/api/courses').success(function (courseList) {
+                    $scope.courseList = courseList;
+                });
+                $scope.newCourse = '';
+                $scope.newGrade = '';
+                $scope.newCourseCredits = '';
+            });
+        };
+
+
+    });
